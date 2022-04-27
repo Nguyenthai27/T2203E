c94eff663c3c4edb5b086267147c347e5cff8a69
Nguyenthai27 <ptejthaii@gmail.com>
2022-04-27 20:18:49 +0700

    EXAMC

--- file thi C/EX2.cpp
+++ file thi C/EX2.cpp
@@ -1,13 +1,11 @@
 #include<stdio.h>
-int daoNguoc(int n,int arr[]){
-	for(int i=0; i< n/2; i++)
-    {
-        int j = n - i - 1;
-        int tmp = arr[j];
-        arr[j] = arr[i];
-        arr[i] = tmp;
+void reverse(int* arr, int size)
+{    
+    for (int i = 0; i < size / 2; ++i) {
+        int temp = arr[i];
+        arr[i] = arr[size - i - 1];
+        arr[size - i - 1] = temp;
     }
-	return i;
 }
 int main(){
 	int n;
@@ -15,11 +13,14 @@ int main(){
 	scanf("%d",&n);
 	int arr[n];
 	for(int i=0;i<n;i++){
-		printf("Phan tu thu %d la:%d");
 		scanf("%d",&arr[i]);
 	}
-	for(int i=0;i<n;i++){
-		int x=daoNguoc(arr[],n);
-		printf("Mang sau khi dao nguoc la:&d",x);
-	}
+	reverse(arr,n);
+		for(int i=0;i<n;i++){
+		printf("Dao nguoc cua mang la: %d\n",arr[i]);
+		}
+		
 }
+
+
+
